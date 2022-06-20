f43302a35cb880532c51d654b9cf82f5c5af897d
n-jabes <nshutij7@gmail.com>
2022-06-20 08:43:22 +0200

    resolving errors

--- server/server.js
+++ server/server.js
@@ -1,16 +1,19 @@
 const express = require('express')
 const bodyparser = require('body-parser');
+const cors = require('cors')
 
 const app = express()
 
 app.use(bodyparser.urlencoded({extended: false}));
 app.use(bodyparser.json());
+app.use(cors());
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('Welcome to the server!')
 })
+
 app.post("/upload_data", (req,res) => {
     let data =req.body.array;
     console.log("Uploaded Data\n\n:" + data);
